9c837e9ba825dc2f52045f12001f3eb9356b4e31
Night Furry <ddhieu2707@gmail.com>
2025-11-18 14:53:49 +0700

    a

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
-let foodArray = ["Cơm sườn","Thịt kho tàu","Hủ tiếu","Bún bò","Bùn riêu","Phở","Táo"]
+let foodArray = [
+    [
+        "Phở",
+        "Cơm tấm",
+        "Hủ tiếu",
+        "Bánh mì",
+        "Bún bò Huế",
+        "Bánh xèo",
+        "Cháo lòng",
+        "Bún chả",
+    ],
+    [
+        "Cơm sườn",
+        "Thịt kho tàu",
+        "Hủ tiếu",
+        "Bún bò",
+        "Bùn riêu",
+        "Phở",
+        "Táo"
+    ],
+    [
+        "Cơm gà",
+        "Gà rán",
+        "Bánh canh",
+        "Bún riêu",
+        "Bánh hỏi",
+        "Mì xào",
+    ],
+    [
+        "Buffet hải sản",
+        "Bún đậu mắm tôm",
+        "Gỏi cá",
+        "Cháo gà",
+        "Bò kho",
+        "Bánh bèo",
+        "Cơm niêu",
+    ],
+]
+let timeMessage = [
+    "Buổi sáng", "Buổi trưa", "Buổi chiều", "Buổi tối"
+]
 let last = 99;
-getRandomFood()
-function getRandomFood() {
-    let random = Math.floor(Math.random() * foodArray.length)
+let flag = 0;
+
+function getHour() {
+    return new Date().getHours()
+}
+
+function setFlag() {
+    let hours = getHour()
+    if (hours> 6 && hours < 12) {
+        flag = 0;
+    } else if (hours >= 12 && hours < 14) {
+        flag = 1;
+    } else if (hours >= 14 && hours < 18) {
+        flag = 2;
+    } else {
+        flag = 3;
+    }
+}
+function setQuestion() {
+    let question = document.querySelector(".request")
+    question.textContent = `${timeMessage[flag]} nên ăn gì?`
+}
+function setRandomFood() {
+    let random = Math.floor(Math.random() * foodArray[flag].length)
     while (random == last) {
-        random = Math.floor(Math.random() * foodArray.length)
+        random = Math.floor(Math.random() * foodArray[flag].length)
     }
     last = random;
-    console.log(random)
     let food = document.querySelector(".food")
-    food.innerHTML = `Hôm nay bạn nên ăn: ${foodArray[random]}`
+    food.textContent = `${timeMessage[flag]} bạn nên ăn: ${foodArray[flag][random]}`
 }
+setFlag()
+setRandomFood()
+setQuestion()
